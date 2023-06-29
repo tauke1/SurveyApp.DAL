819f08fe49b03bb126ccd109db9cab54c5081224
Tauke Abdrakhmanov <tauke1994@gmail.com>
2023-06-29 18:38:29 +0600

    small fix

--- Models/SurveyQuestionPattern.cs
+++ Models/SurveyQuestionPattern.cs
@@ -11,7 +11,8 @@ namespace SurveyApp.DAL.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public SurveyQuestionInputType InputTypeId { get; set; }
+        public int InputTypeId { get; set; }
+        public SurveyQuestionInputType InputType { get; set; }
         public List<SurveyQuestionPatternRequiredParameter> RequiredParameters { get; set; }
     }
 }
